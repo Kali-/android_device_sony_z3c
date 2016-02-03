cbe6195f58404af32a5231da6b531a0750a394d8
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2016-02-03 18:45:37 +0100

    aosp
    Change-Id: I3e7ddb333e8ea7344ed50c99912519ad9f58f47f

--- extract-files.sh
+++ extract-files.sh
@@ -7,7 +7,8 @@ FP=$(cd ${0%/*} && pwd -P)
 export VENDOR=$(basename $(dirname $FP))
 export DEVICE=$(basename $FP)
 export BOARDCONFIGVENDOR=true
+export TARGET_DEVICE=$DEVICE
 
 ../common/extract-files.sh $@
-
 ../common/setup-makefiles.sh
+./setup-makefiles.sh
